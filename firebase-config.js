a6efbe54de69a27fecf90eb3d214d5bc4fdb93a3
Rayan76-a11y <lashay.yudhkar.sy@gmail.com>
2026-07-18 07:15:42 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUuqh4sdhtaUaVeKunBz0oTUFrLIjzmME",
+  authDomain: "rayan-6a983.firebaseapp.com",
+  projectId: "rayan-6a983",
+  storageBucket: "rayan-6a983.firebasestorage.app",
+  messagingSenderId: "330229582505",
+  appId: "1:330229582505:web:dc9b472e81f63e71af37b8",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "daleel-ajanib-v2";
+const ASSETS = [
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try to fetch the latest page/asset from the server.
+// Only fall back to the cached copy if there's no internet connection.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
